f362b1d553d029789ef445ff9fa7105d49f1f8c5
杨 珂 <yangke@hualala.com>
2017-11-10 14:26:17 +0800

    init

---
+++ app/config/config.staging.js
@@ -0,0 +1,13 @@
+/**
+ * @Author: Xiao Feng Wang  <xf>
+ * @Date:   2017-07-19T15:50:55+08:00
+ * @Email:  wangxiaofeng@hualala.com
+ * @Filename: config.staging.js
+ * @Last modified by:   xf
+ * @Last modified time: 2017-07-19T16:41:06+08:00
+ * @Copyright: Copyright(c) 2017-present Hualala Co.,Ltd.
+ */
+
+export const host = 'http://hll_mall:9000/web-service'
+export const APIHost = 'http://hll_mall:9000/API'
+export const web_service = 'web_service'
---
+++ app/config/index.js
@@ -0,0 +1,21 @@
+/**
+ * @Author: Xiao Feng Wang  <xf>
+ * @Date:   2017-07-19T15:50:20+08:00
+ * @Email:  wangxiaofeng@hualala.com
+ * @Filename: index.js
+ * @Last modified by:   xf
+ * @Last modified time: 2017-07-19T16:39:16+08:00
+ * @Copyright: Copyright(c) 2017-present Hualala Co.,Ltd.
+ */
+
+import {
+  host,
+  APIHost,
+  WEB_SERVICE,
+} from './config._build_.js'
+
+export {
+  host,
+  APIHost,
+  WEB_SERVICE,
+}
